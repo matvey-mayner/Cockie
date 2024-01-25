84d5ba1f16a40463fcea1d37151c7e147265c67e
matvey-mayner <144268527+matvey-mayner@users.noreply.github.com>
2024-01-25 19:07:12 +0300

    Update Cookie.lua

--- Cookie.lua
+++ Cookie.lua
@@ -1,1 +1,13 @@
+local shell = require("shell")
 
+if command == "COOCKIE" then
+  shell.execute("Coockie.lua")
+computer.shutdown()
+  
+print("!IM WANT COOKIE!")
+while true do
+  printCommands()
+  io.write(" ")
+  local command = io.read()
+  handleCommand(command)
+end
